b1f3327e4efcbb1be75e0fe6035ca5de43c6ac17
KBTREY35 <kevinbhangu12@gmail.com>
2024-11-02 19:18:57 -0400

    exec9 commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p>101418717</p>
+        <p>Kevin Bhangu</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
